c36606db4050143c5ba5ddce5ec532f9036652d4
lucgbrl <lucassantos@alu.ufc.br>
2019-02-22 08:41:37 -0300

    os botões flutuantes foram adicionados à página de conteúdo

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
    $(document).ready(function(){
-    
+
     // as componentes de votação e gráfico são ocultadas da visualização pelo jQuery
     // ambas são otimizadas a partir de manipulações com o CSS para uma animação mais satisfatória
-    
+
     $("#b").hide();
     $("#card").hide();
-    
+
     // As funções abaixo são responsáveis por: 1 - exibir os botões de votação
     // 2 - Exibir as barras de progresso após votar em uma das opções
 
-    $("#menu-toggle").click(function(){
+    $("#menu-toggle").id(":hover")(function(){
         $("#b").slideToggle(4);
     });
-    
+
     $("#b").click(function(){
         $("#card").show(4);
     });
